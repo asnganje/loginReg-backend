cca24bc9336c0a0540b0ab1bf20f74fe977a4a39
asnganje <abd.nganje@gmail.com>
2024-01-22 06:15:00 +0300

    change name from user to auth

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./server/db/connect");
-const userRoute = require("./server/routes/userRoutes");
+const authRoute = require("./server/routes/authRoutes");
 const app = express();
 
 
 
-app.use('/api/v1/auth', userRoute)
+app.use('/api/v1/auth', authRoute)
 
 const port = process.env.PORT || 3000;
 const url = process.env.MONGO_URI
