68acaa166f28278abe459479f3d8df359964a5bb
asnganje <abd.nganje@gmail.com>
2024-01-22 06:20:10 +0300

    pass controllers to routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,9 @@
-const express = require('express')
+const express = require('express');
+const { getAllUsers, register, login } = require('../controllers/authControllers');
 const router = express.Router()
 
-router.route('/').get().post()
+router.route('/register').post(register)
+router.route('/login').post(login)
+router.route('/').get(getAllUsers)
+
 module.exports = router;
