ab7fbba1e57d61637b47368bb9cdd3d0d427b7f0
asnganje <abd.nganje@gmail.com>
2024-01-22 06:08:24 +0300

    run connectDB in app

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./server/db/connect");
+const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+const url = process.env.MONGO_URI
 
-const start = () => {
-    try {
-        app.listen(port, ()=> {
-            console.log(`Server is listening on port ${port}...`);
-        })
-    } catch (error) {
-        console.log('Server could not be loaded');
-    }
-}
+const start = async () => {
+  try {
+    await connectDB(url)
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log("Server could not be loaded");
+  }
+};
 
-start()
-
+start();
