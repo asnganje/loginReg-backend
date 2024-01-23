4bf47d307cdb6be3f558ca66468e67711808153c
asnganje <abd.nganje@gmail.com>
2024-01-23 16:11:01 +0300

    add cors logic to app

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const connectDB = require("./server/db/connect");
 const authRoute = require("./server/routes/authRoutes");
 const app = express();
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1/auth', authRoute)
 
